8abb8db6dd45fcb4fa949ef68e6e2bdff5a19999
ryota.sakamoto <ryota.sakamoto@aijus.com>
2018-03-21 11:04:08 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,34 @@
+extern crate clap;
+
 use std::path::Path;
 use std::fs;
+use clap::{App, Arg};
 
 fn main() {
-    println!("> ls .");
-    ls(".");
+    let app = App::new("ls").arg(Arg::with_name("all").help("show all").short("a"));
+    let mathces = app.get_matches();
+    let is_show_all = mathces.is_present("all");
 
-    println!("> ls src");
-    ls("src");
+    println!("> ls {}.", if is_show_all {
+        "-a "
+    } else {
+        ""
+    });
+    ls(".", is_show_all);
+
+    println!("> ls {}src", if is_show_all {
+        "-a "
+    } else {
+        ""
+    });
+    ls("src", is_show_all);
 }
 
-fn ls(p: &str) {
+fn ls(p: &str, is_show_all: bool) {
     let mut result = String::new();
     let end = if true {
         "\n"
-        // -l
+    // -l
     } else {
         " "
     };
@@ -22,12 +37,11 @@ fn ls(p: &str) {
     if path.is_dir() {
         for file in fs::read_dir(path).unwrap() {
             let file = file.unwrap();
-            
+
             let file_name = file.file_name();
             let name = file_name.to_str().unwrap();
 
-            // -a
-            if !name.starts_with(".") {
+            if is_show_all || !name.starts_with(".") {
                 result += &format!("{}{}", name, end);
             }
         }
